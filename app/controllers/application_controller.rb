c621c6283cce9fa7cf2cfbf2cc2da056ca6ac9f7
wuttisaks <57160545@go.buu.ac.th>
2016-10-29 15:24:43 +0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
   def hello
 	render html: "hello, world!"
   end
+	
   def goodbye
-	  render html: "goodbye world!"
+	render html: "goodbye world!"
   end
+	
 end
